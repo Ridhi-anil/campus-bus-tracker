4b96f90315957116d94ae3b00764a10e5a78b328
Ridhi-anil <ridhianilpnr@gmail.com>
2026-01-13 23:49:05 +0530

    header section complete

---
+++ src/Header.jsx
@@ -0,0 +1,10 @@
+export default function Header() {
+  return (
+    <header className="header">
+      <h2>
+        <mark class="yellow">Campus</mark> Bus Tracker
+      </h2>
+      <p>Real-time driver location .Tracking for your convenience</p>
+    </header>
+  );
+}
